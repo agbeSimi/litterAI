2ecbfc86bae5395e9d2da4ca19f1ee35f08be9f8
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-27 11:02:38 +0200

    premiere affichage di ptojet

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Accueil from './pages/Accueil';
+
+function App() {
+  return (
+    <div className="App">
+      <Accueil />
+    </div>
+  );
+}
+
+
+export default App;
