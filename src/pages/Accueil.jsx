b468c953e315c36c69fd74b4fa3b07de9ebdf5d8
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-27 15:22:13 +0200

    ajout d'un bouton et remplacement du contenu des cartes par leur composants respectifs

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import logoRobot from '../assets/logo_robot.png';
+import CartePresentation from "../composants/CarteConcept.jsx";
+import CarteConcept from "../composants/CarteConcept.jsx";
+import CarteExplication from "../composants/CarteExplication.jsx";
 
 function Accueil() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center p-8">
-      <header className="text-center mb-5">
+    <div className="text-center mb-5 min-h-screen bg-gray-50 flex flex-col items-center p-8">
         <img
           src={logoRobot}
           alt="LitterAl"
           className="mb-4"
           style={{ width: '120px' }}
         />
-        <h1 className="display-4 fw-bold text-primary">Litter AI</h1>
-        <h2 className="text-secondary">Objectif : Savoir isoler "x" et résoudre l'équation</h2>
-      </header>
-
-
+        <h1 className="display-4 fw-bold text-primary mb-5">Litter AI</h1>
+        <h2 className="text-secondary mb-5">Chapitre : Les équations</h2>
+        <h4 className="fw-bold text-secondary">Objectif : Savoir isoler "x" et résoudre l'équation</h4>
+        <CarteConcept/>
+        <CarteExplication/>
+      <div className="text-center mt-5">
+        <button className="btn btn-primary rounded-pill border-0 px-5 py-3 fw-bold shadow">
+          Commencer la séance
+        </button>
+      </div>
     </div>
   );
-};
+}
 
 export default Accueil;
