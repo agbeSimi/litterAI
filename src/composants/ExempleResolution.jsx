f7102699d3861e386963dd6245fef580cc9d654f
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-29 10:52:51 +0200

    ajout d'un tableau avec les donn"es qu'on souhatie ajouter 1 par 1 apres chasue clique du bouton suivant et implementation du composant bouton

--- src/composants/ExempleResolution.jsx
+++ src/composants/ExempleResolution.jsx
@@ -1,8 +1,12 @@
 import React, {useEffect, useState} from "react";
 import {motion} from "framer-motion";
+import BoutonSuivant from "./BoutonSuivant.jsx";
 
 function ExempleResolution() {
   const [equation, setEquation] = useState({ a: 0, b: 0, c: 0, x: 0 });
+  const[indexEtape, setIndexEtape] = useState(1);
+  let nvlVal;
+
 
   function genererEquationAleatoire(){
     const valA = Math.floor(Math.random() * 4) + 2;
@@ -19,10 +23,37 @@ function ExempleResolution() {
     genererEquationAleatoire();
   }, []);
 
+  const etapes = [
+    { id: 1,
+      jsx:
+        <p className="mb-2">1. Équation : <span className="text-info">{equation.a}x + {equation.b} = {equation.c}</span></p>
+    },
+    { id: 2,
+      jsx:
+        <p className="mb-4 text-warning ms-3">→ L'opposé de <span className="fw-bold">+{equation.b}</span> est <span className="fw-bold">-{equation.b}</span></p>
+    },
+    { id: 3, jsx:
+        <p className="mb-2">2. On obtient : <span className="text-info">{equation.a}x = { nvlVal = equation.c - equation.b}</span></p>
+    },
+    { id: 4,
+      jsx:
+        <p className="mb-4 text-warning ms-3">→ La réciproque de <span className="fw-bold">×{equation.a}</span> est <span className="fw-bold">÷{equation.a}</span></p>
+    },
+    { id: 5,
+      jsx:
+        <p className="mb-0 fs-5">3. Solution : <span className="text-success fw-bold">x = {nvlVal / equation.a}</span> ✅</p>
+    }
+  ];
+
+  function etapeSuivante() {
+    if (indexEtape < etapes.length) {
+      setIndexEtape(indexEtape + 1);
+    }
+  }
   //valeur calculer dans l'exemple
-  let nvlVal;
+
   return (
-    <motion.div
+    <>    <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.7 }}
@@ -30,19 +61,24 @@ function ExempleResolution() {
       className="card bg-dark text-light border-0 rounded-4 p-4 mb-5 shadow-sm"
     >
       <div className="card-body font-monospace"> {/* Police monospace pour le style code */}
-
-        {/* Étape 1 */}
-        <p className="mb-2">1. Équation : <span className="text-info">{equation.a}x + {equation.b} = {equation.c}</span></p>
-        <p className="mb-4 text-warning ms-3">→ L'opposé de <span className="fw-bold">+{equation.b}</span> est <span className="fw-bold">-{equation.b}</span></p>
-
-        {/* Étape 2 */}
-        <p className="mb-2">2. On obtient : <span className="text-info">{equation.a}x = { nvlVal = equation.c - equation.b}</span></p>
-        <p className="mb-4 text-warning ms-3">→ La réciproque de <span className="fw-bold">×{equation.a}</span> est <span className="fw-bold">÷{equation.a}</span></p>
-
-        {/* Solution */}
-        <p className="mb-0 fs-5">3. Solution : <span className="text-success fw-bold">x = {nvlVal / equation.a}</span> ✅</p>
+        {etapes.slice(0, indexEtape).map((etape) => (
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.7,
+                          ease:"easeOut"}}
+            key={etape.id} className="mb-3 ">
+            {etape.jsx}
+          </motion.div>
+        ))}
       </div>
+
+
     </motion.div>
+    <BoutonSuivant
+      onClick={etapeSuivante}
+      visible={indexEtape<etapes.length}/>
+    </>
 
   );
 }
