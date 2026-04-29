8fbb6b66361ef913e8beeaab54a60558b495bf0c
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-29 11:03:09 +0200

    .

--- src/composants/BoutonSuivant.jsx
+++ src/composants/BoutonSuivant.jsx
@@ -5,7 +5,7 @@ function boutonSuivant({onClick, visible}) {
     return (
       <div className="d-flex justify-content-center"> {/* Pour centrer le bouton sous le bloc */}
         <button
-          className="btn btn-primary rounded-circle shadow d-flex align-items-center justify-content-center "
+          className="btn btn-primary border-0 rounded-circle shadow d-flex align-items-center justify-content-center "
           onClick={onClick}
           style={{ width: '50px', height: '50px' }} // On force la taille pour faire un cercle parfait
         >
