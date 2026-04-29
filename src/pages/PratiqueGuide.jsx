64353b64dcd865600dcc2c24db5d3604a732e760
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-29 15:23:11 +0200

    nouvelle page pratique guidée

---
+++ src/pages/PratiqueGuide.jsx
@@ -0,0 +1,12 @@
+function PratiqueGuide() {
+  return(
+    <div className="container py-5 min-h-screen bg-light">
+      <div className="row justify-content-center">
+        <div className="col-lg-8 bg-white p-5 shadow-lg rounded-4">
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PratiqueGuide;
