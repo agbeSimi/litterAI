5a7692a93fbe2e60b2f20ffcc521367dec348518
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-29 16:34:02 +0200

    requete vers l'api donc l'ia pour generer une equation

--- src/pages/PratiqueGuide.jsx
+++ src/pages/PratiqueGuide.jsx
@@ -1,8 +1,47 @@
+import {useState} from "react";
+
 function PratiqueGuide() {
+  const [conversation, setConversation] = useState([]);
+  const [isWorking, setIsWorking] = useState(false);
+
+  function lancerExercice() {
+    const URL_BASE = 'https://127.0.0.1:8000/api'
+    setIsWorking(true);
+    fetch(`${URL_BASE}/ia/groq`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        question: "Génère une équation simple et demande-moi la première étape.",
+        listeMessages: conversation
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        setConversation(data.listeMessages);
+        setIsWorking(false);
+      })
+      .catch(error => {
+        console.error("Aïe :", error);
+        setIsWorking(false);
+      });
+  }
+
   return(
     <div className="container py-5 min-h-screen bg-light">
       <div className="row justify-content-center">
         <div className="col-lg-8 bg-white p-5 shadow-lg rounded-4">
+
+          <button
+            className="btn btn-primary"
+            onClick={lancerExercice}
+            disabled={isWorking}
+          >
+            {isWorking ? "L'IA réfléchit..." : "Commencer l'exercice"}
+          </button>
+
         </div>
       </div>
     </div>
