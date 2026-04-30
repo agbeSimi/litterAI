56b842edaff61225554cc00fb32bad5573518b20
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-30 12:07:43 +0200

    nouvelle fonction qui gere l'envoi des messages de l'eleve

--- src/services/LitterAI_API.js
+++ src/services/LitterAI_API.js
@@ -1,5 +1,7 @@
-function lancerExercice(conversation, setConversation, setIsWorking) {
-  const URL_BASE = 'https://127.0.0.1:8000/api'
+
+const URL_BASE = 'https://127.0.0.1:8000/api'
+
+export function lancerExercice(conversation, setConversation, setIsWorking) {
   setIsWorking(true);
   fetch(`${URL_BASE}/ia/groq`, {
     method: 'POST',
@@ -8,7 +10,7 @@ function lancerExercice(conversation, setConversation, setIsWorking) {
       'Accept': 'application/json',
     },
     body: JSON.stringify({
-      question: "Souhaite moi le bienvenue dans la 'Pratique guidé' et Génère une équation simple de format aX + b = c et demande-moi la première étape, sans me donner aucune indice ou réponse.",
+      question: "Souhaite moi le bienvenue dans la 'Pratique guidé' et Génère une équation aléatoire simple de format aX + b = c et demande-moi la première étape, sans me donner aucune indice ou réponse.",
       listeMessages: conversation
     })
   })
@@ -23,4 +25,33 @@ function lancerExercice(conversation, setConversation, setIsWorking) {
     });
 }
 
-export default lancerExercice;
+
+
+export function envoyerMessage(conversation, setConversation, input, setInput, setIsWorking) {
+  const inputEleve = { role: 'user', content: input };
+
+  const convMisAJour = [...conversation, inputEleve];
+  setInput("");
+
+  setIsWorking(true);
+  fetch(`${URL_BASE}/ia/groq`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        question: input,
+        listeMessages: conversation
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        setConversation(data.listeMessages);
+        setIsWorking(false);
+      })
+      .catch(error => {
+        console.error("Aïe :", error);
+        setIsWorking(false);
+      });
+}
