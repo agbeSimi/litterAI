cdac39eb31d4ce009e21bc9fc05f04a34f952f04
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-04-30 12:09:19 +0200

    affichage de la conversation entre l'utilisatuer et l'ia

--- src/pages/PratiqueGuide.jsx
+++ src/pages/PratiqueGuide.jsx
@@ -1,28 +1,47 @@
 import {useState} from "react";
-import lancerExercice from "../services/LitterAI_API.js";
+import {lancerExercice, envoyerMessage} from "../services/LitterAI_API.js";
 
 function PratiqueGuide() {
   const [conversation, setConversation] = useState([]);
   const [isWorking, setIsWorking] = useState(false);
-
+  const[input, setInput] = useState("");
   return(
     <div className="container py-5 min-h-screen bg-light">
       <div className="row justify-content-center">
         <div className="col-lg-8 bg-white p-5 shadow-lg rounded-4">
 
-          {conversation.filter(message => message.role !== 'user').map((message, index) => (
+          {conversation?.filter((_, index) => index !== 0).map((message, index) => (
             <div key={index} style={{ whiteSpace: 'pre-wrap' }}>
               {message.content}
             </div>
           ))}
 
-          {conversation.length === 0 && <button
+          {(conversation?.length || 0) === 0 && <button
             className="btn btn-primary"
             onClick={() => lancerExercice(conversation, setConversation, setIsWorking)}
             disabled={isWorking}
           >
             {isWorking ? "L'IA réfléchit..." : "Commencer l'exercice"}
           </button>}
+          <div>
+            {conversation?.length > 0 && <input
+            type="text"
+            className="form-control mb-3"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            disabled={isWorking}/>
+            }
+
+            {conversation?.length > 0 && <button
+              type="submit"
+              className="btn btn-primary border-0"
+              disabled={isWorking}
+              onClick={() => envoyerMessage(conversation,setConversation,input,setInput,setIsWorking)}
+            >
+            Envoyer
+            </button>
+            }
+          </div>
 
         </div>
       </div>
