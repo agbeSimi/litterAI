541a0a5860537317bfce0664e646a334d1abb54f
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-04 15:24:43 +0200

    page credit creer

---
+++ src/pages/Credit.jsx
@@ -0,0 +1,61 @@
+import {motion} from "framer-motion";
+
+
+function Credit() {
+  return(
+    <div className="d-flex flex-column min-vh-100 bg-gray-50">
+      {/* Header avec le logo pour revenir à l'accueil */}
+      <main className="flex-grow-1 container py-5">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="mx-auto"
+          style={{ maxWidth: '800px' }}
+        >
+          <div className="bg-white shadow-sm rounded-4 p-5">
+            <h2 className="fw-bold text-primary mb-4 border-bottom pb-2">L'Équipe</h2>
+            <div className="mb-5">
+              <p className="mb-2">
+                <strong>Conception Pédagogique :</strong> Morgan MARTIN (Professeur Agrégé de Mathématiques)
+              </p>
+              <p>
+                <strong>Développement Logiciel :</strong> Simisola Agbe
+              </p>
+            </div>
+
+            <h2 className="fw-bold text-primary mb-4 border-bottom pb-2">La Technologie (La "Stack")</h2>
+            <div className="mb-5">
+              <ul className="list-unstyled">
+                <li className="mb-3 d-flex align-items-start">
+                  <i className="bi bi-cpu-fill text-primary me-3 mt-1"></i>
+                  <div>
+                    <strong>IA :</strong> Utilisation du modèle de langage <strong>Llama 3</strong> via l'API <strong>GROQ</strong> pour une vitesse de réponse instantanée.
+                  </div>
+                </li>
+                <li className="d-flex align-items-start">
+                  <i className="bi bi-server text-primary me-3 mt-1"></i>
+                  <div>
+                    <strong>Backend :</strong> Serveur sécurisé symfony.
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+            <h2 className="fw-bold text-primary mb-4 border-bottom pb-2">Engagement RGPD </h2>
+            <div className="bg-light card-litteral p-3 rounded-3 border-start border-success border-4">
+              <p className="mb-0 text-dark">
+                LitterAl est conçu pour être un outil <strong>totalement anonyme</strong>. Aucune donnée personnelle n'est collectée : nous utilisons uniquement des pseudos ou des codes de séance pour garantir la protection de ta vie privée. 
+              </p>
+            </div>
+          </div>
+        </motion.div>
+      </main>
+
+      <footer className="py-4 text-center text-muted">
+        <small>© 2026 - Projet LitterAl </small>
+      </footer>
+    </div>
+  )
+}
+
+export default Credit;
