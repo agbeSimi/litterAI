4c7d69505422ac90725c4f7f9766d06bf03fbcd5
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-04 16:30:10 +0200

    nouvelle utilisation des composants

--- src/pages/Equation/pageEquation.jsx
+++ src/pages/Equation/pageEquation.jsx
@@ -3,6 +3,7 @@ import logoRobot from '../../assets/logo_robot.png';
 import CarteConcept from "../../composants/CarteConcept.jsx";
 import CarteExplication from "../../composants/CarteExplication.jsx";
 import { useNavigate } from "react-router-dom";
+import React from "react";
 
 function PageEquation() {
   const navigation = useNavigate();
@@ -21,8 +22,12 @@ function PageEquation() {
         <h1 className="display-4 fw-bold text-primary mb-5 mt-3">Litter AI</h1>
         <h2 className="text-secondary mb-5">Chapitre : Les équations</h2>
         <h4 className="fw-bold text-secondary">Objectif : Savoir isoler "x" et résoudre l'équation</h4>
-        <CarteConcept/>
-        <CarteExplication/>
+        <CarteConcept
+          description="Une équation est une balance en équilibre. Pour isoler x, chaque opération effectuée d'un côté doit
+        être effectuée de l'autre."/>
+        <CarteExplication
+          description="Il faut savoir utiliser l'opposé (Annuler une addition par une soustraction et inversement)
+            ainsi que la réciproque(Annuler une multiplication par une division et inversement)"/>
       <div className="text-center mt-5">
         <button onClick={goToModelage} className="btn btn-primary rounded-pill border-0 px-5 py-3 fw-bold shadow">
           Commencer la séance
