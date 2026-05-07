74af1d1c334ddf13a1a9b80e39ab228b3fa349f5
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-07 13:19:47 +0200

    modif de prompt

--- src/pages/Equation/pratiqueGuide/PratiqueGuide3.jsx
+++ src/pages/Equation/pratiqueGuide/PratiqueGuide3.jsx
@@ -52,7 +52,7 @@ function PratiqueGuide2() {
                          2. CONTRAINTES STRICTES : 
                             - La solution x DOIT être un nombre entier (ex: 1, 2, 3...).
                             - Les coefficients a et b DOIVENT être des nombres décimaux (ex: 1.5, 0.5, 2.5).
-                            - Utilise toujours un point (.) pour la virgule.
+                            - Utilise toujours un point (.) pour la virguledans toute l'equation.
                          3. MÉTHODE : Guide-moi pas à pas. Ne donne jamais la réponse. Commence par me demander quelle opération faire pour déplacer b de l'autre côté de l'égalité.
                          4. RÈGLE D'AFFICHAGE : Si tu obtiens 1x, écris "x" directement.`) }
                                         >
