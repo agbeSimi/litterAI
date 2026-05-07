5a485fc8011b3e6d33c8bb19d442fb584b28125a
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-07 11:06:46 +0200

    nouvelle redirection vers la nouvelle page de pratique guidé

--- src/pages/Equation/Modelage/Modelage2.jsx
+++ src/pages/Equation/Modelage/Modelage2.jsx
@@ -71,7 +71,7 @@ function Modelage2() {
                   scale: 1.02
                 }}
               >
-                <button onClick={goToPratiqueGuide} className="btn btn-sm px-md-5 py-md-3 px-4 rounded-pill fw-bold d-flex flex-wrap justify-content-center gap-3 mt-4" style={{ backgroundColor: 'deepskyblue', color: 'white' }}>
+                <button onClick={() => navigate("/pratiqueGuide2")} className="btn btn-sm px-md-5 py-md-3 px-4 rounded-pill fw-bold d-flex flex-wrap justify-content-center gap-3 mt-4" style={{ backgroundColor: 'deepskyblue', color: 'white' }}>
                   Pratique Guidée
                 </button>
               </motion.div>
