67492913aaaef6c70541f2ac88605d6532586ee7
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-11 10:29:56 +0200

    creation d'une foncition qui va nous generer un programme de calcul pour l'eleve

--- src/pages/exoInteractif/pratiqueAutonomeInteractif.jsx
+++ src/pages/exoInteractif/pratiqueAutonomeInteractif.jsx
@@ -4,7 +4,7 @@ function pratiqueAutonomeInteractif() {
   const [donnees, setDonnees] = useState(null);
 
   const [progression, setProgression] = useState(1);
-
+  const [currentEquation, setCurrentEquation] = useState(genererProgramme());
   const [operationPlacer, setOperationPlacer] = useState(null);
   const [resultatSaisi, setResultatSaisi] = useState("");
 
@@ -12,27 +12,64 @@ function pratiqueAutonomeInteractif() {
 
   
 
-  
-  function creerExercice()
-  {
-    const nombreDepart = Math.floor(Math.random() * 10) + 2;
-    for (let i = 0; i < 3; i++) {
-      const nouvelleEtape = genererEtape(nombreEnCours);
-     
+  function genererProgramme() {
+    const operations = ['additionner', 'soustraire']
+    const operationFinal = ['multiplier', 'diviser']
+    const indexAleatoire = Math.floor(Math.random() * operations.length);
+    let a = Math.floor(Math.random() * 8) + 2;
+    let b = Math.floor(Math.random() * 9) + 1;
+    let x = Math.floor(Math.random() * 5) + 1;
+    let c = Math.floor(Math.random() * 9) + 1;
+    const operation = () => {
+      if (indexAleatoire === 0)
+        return `+`
+      else return `-`
+    }
+    const operationFinale = () => {
+      if (indexAleatoire === 0)
+        return `*`
+      else return `/`
+    }
+    const premierePartie = `Multiplier par ${a}`
+    const deuxiemePartie = `${operations[indexAleatoire]} ${b}`
+    const troisiemePartie = `${operationFinal[indexAleatoire]} par ${c}`
+    let resultatPremiereEtape = x * a;
+    let resultatDeuxiemeEtape ;
+    if (operation() === "+") {
+      resultatDeuxiemeEtape = resultatPremiereEtape + b;
+    } else if (operation() === "-") {
+      resultatDeuxiemeEtape = resultatPremiereEtape - b;
+    }
+
+    let resultatTroisiemeEtape;
+
+    if (operationFinale() === "*") {
+      resultatTroisiemeEtape = resultatDeuxiemeEtape * c;
+    } else if (operation() === "/") {
+      resultatTroisiemeEtape = resultatDeuxiemeEtape / c;
     }
 
 
+  return{
+    affichage: `${a} * x ${operation()} ${b} ${operationFinale()} ${c}`,
+    solutions: [resultatPremiereEtape,resultatDeuxiemeEtape,resultatTroisiemeEtape],
+    x: x,
+    a: a,
+    b: b,
+    c:c,
+    premierePartie: premierePartie,
+    deuxiemePartie: deuxiemePartie,
+    troisiemePartie: troisiemePartie,
+
   }
 
+  }
 
   const etapes = [
     {
       consigne:
     }
   ]
-
-  return();
-
 
 }
 export default pratiqueAutonomeInteractif;
