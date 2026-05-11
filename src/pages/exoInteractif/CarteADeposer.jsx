e3007eae0f535a563ab92ab6581203c0e15926fd
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-11 13:25:51 +0200

    configuration de la carte a deplacer avec dnd kit

---
+++ src/pages/exoInteractif/CarteADeposer.jsx
@@ -0,0 +1,16 @@
+import {useDraggable} from "@dnd-kit/core";
+
+function CarteADeposer({id, content}) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({ id });
+  const style = transform ? {
+    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  } : undefined;
+
+  return (
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes} className="card-bleue">
+      {content}
+    </div>
+  )
+}
+
+export default CarteADeposer;
