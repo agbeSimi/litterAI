72400ef34eb2176810b62c3a00e652b0204d29c9
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-11 10:49:32 +0200

    ajout d'une verification pour s'assuere que en cas de division on ai un nombre entier a la fin de la division

--- src/pages/exoInteractif/pratiqueAutonomeInteractif.jsx
+++ src/pages/exoInteractif/pratiqueAutonomeInteractif.jsx
@@ -19,7 +19,8 @@ function pratiqueAutonomeInteractif() {
     let a = Math.floor(Math.random() * 8) + 2;
     let b = Math.floor(Math.random() * 9) + 1;
     let x = Math.floor(Math.random() * 5) + 1;
-    let c = Math.floor(Math.random() * 9) + 1;
+    let c;
+
     const operation = () => {
       if (indexAleatoire === 0)
         return `+`
@@ -30,22 +31,42 @@ function pratiqueAutonomeInteractif() {
         return `*`
       else return `/`
     }
-    const premierePartie = `Multiplier par ${a}`
-    const deuxiemePartie = `${operations[indexAleatoire]} ${b}`
-    const troisiemePartie = `${operationFinal[indexAleatoire]} par ${c}`
-    let resultatPremiereEtape = x * a;
-    let resultatDeuxiemeEtape ;
     if (operation() === "+") {
       resultatDeuxiemeEtape = resultatPremiereEtape + b;
     } else if (operation() === "-") {
       resultatDeuxiemeEtape = resultatPremiereEtape - b;
     }
+    if (operationFinale() === '/') {
+      // On cherche un diviseur de resultatDeuxiemeEtape
+
+      // Pour faire simple : on essaie des chiffres jusqu'à ce qu'on en trouve un qui marche
+      let diviseursPossibles = [];
+      for (let i = 2; i <= 10; i++) {
+        if (resultatDeuxiemeEtape % i === 0) {
+          diviseursPossibles.push(i);
+        }
+      }
+      // Si on a trouvé des diviseurs, on en prend un au hasard, sinon on prend 1
+      c = diviseursPossibles.length > 0
+        ? diviseursPossibles[Math.floor(Math.random() * diviseursPossibles.length)]
+        : 1;
+
+    } else {
+      c = Math.floor(Math.random() * 9) + 1;
+    }
+
+    const premierePartie = `Multiplier par ${a}`
+    const deuxiemePartie = `${operations[indexAleatoire]} ${b}`
+    let resultatPremiereEtape = x * a;
+    let resultatDeuxiemeEtape ;
+    const troisiemePartie = `${operationFinal[indexAleatoire]} par ${c}`
+
 
     let resultatTroisiemeEtape;
 
     if (operationFinale() === "*") {
       resultatTroisiemeEtape = resultatDeuxiemeEtape * c;
-    } else if (operation() === "/") {
+    } else if (operationFinale() === "/") {
       resultatTroisiemeEtape = resultatDeuxiemeEtape / c;
     }
 
