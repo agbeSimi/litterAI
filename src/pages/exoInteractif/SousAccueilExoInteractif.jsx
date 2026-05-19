db07d08cdecd924adf36953ee79c7c89bb8e099e
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-19 11:49:28 +0200

    utilisation du composant sou accueil

---
+++ src/pages/exoInteractif/SousAccueilExoInteractif.jsx
@@ -0,0 +1,11 @@
+import SousAccueilComposant from "../sousAccueil.jsx";
+
+function SousAccueilExoInteractif() {
+  return (
+    <SousAccueilComposant titre="Le programme intéractif  🧩"
+    pathParcourComplet="/modelageInteractif"
+    />
+  )
+}
+
+export default SousAccueilExoInteractif;
