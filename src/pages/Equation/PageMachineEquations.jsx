b1eb375d15b8c8cc2a044986a7d1915d0d6d8871
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-19 16:42:52 +0200

    changement du nom des niveaux

--- src/pages/Equation/PageMachineEquations.jsx
+++ src/pages/Equation/PageMachineEquations.jsx
@@ -4,8 +4,8 @@ function PageMachineEquations() {
   // 1. Tu définis la liste des niveaux de ce chapitre
   const niveauxEquations = [
     { id: 1, label: "Niveau 1 — Equations simples", pathRoute: "/pratiqueAutonome" },
-    { id: 2, label: "Niveau 2 — Equations de 2 côtés", pathRoute: "/pratiqueAutonome2" },
-    { id: 3, label: "Niveau 3 — Equation a decimal", pathRoute: "/pratiqueAutonome3" }
+    { id: 2, label: "Niveau 2 — Equations linéaire", pathRoute: "/pratiqueAutonome2" },
+    { id: 3, label: "Niveau 3 — Equation a nombres décimaux", pathRoute: "/pratiqueAutonome3" }
   ];
 
   // 2. Tu injectes ces données dans ton composant universel
