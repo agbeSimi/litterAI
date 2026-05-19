e7877dcb06759e2b48cd44335ad588edf9ee9ab3
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-19 17:34:53 +0200

    configuration machine de tester les egalité

---
+++ src/pages/testerEgalite/pageMachineTesterEgalite.jsx
@@ -0,0 +1,19 @@
+import MachineExo from "../../MachineExo.jsx"; // Assure-toi que le chemin est correct
+
+function PageMachineTesterEgalite() {
+  // 1. Tu définis la liste des niveaux de ce chapitre
+  const niveauxEquations = [
+    { id: 1, label: "Niveau 1 — Multiplications simples", pathRoute: "/PratiqueAutonomeTesterEgalite1" },
+    { id: 2, label: "Niveau 2 — Nombres décimaux", pathRoute: "/PratiqueAutonomeTesterEgalite2" },
+    { id: 3, label: "Niveau 3 — Deux variables (x, y)", pathRoute: "/PratiqueAutonomeTesterEgalite3" }  ];
+
+  // 2. Tu injectes ces données dans ton composant universel
+  return (
+    <MachineExo
+      titreModule="Chapitre : Tester une égalité"
+      listeNiveaux={niveauxEquations}
+    />
+  );
+}
+
+export default PageMachineTesterEgalite;
