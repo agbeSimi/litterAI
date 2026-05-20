5d057ff90d9869706eb5892fb823dde383e9b737
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-20 09:57:46 +0200

    implementation de la machine a exercice

---
+++ src/pages/CalculerDeveloppement/SousAccueilCalculerDeveloppement.jsx
@@ -0,0 +1,12 @@
+import SousAccueilComposant from "../../composants/sousAccueilComposant.jsx";
+
+function SousAccueilCalculerDeveloppement() {
+  return (
+    <SousAccueilComposant titre="Tester une égalité  🔎"
+    pathParcourComplet="/IntroductionCalculerDeveloppement"
+    pathMachine="/PageMachineCalculerDeveloppement"
+    />
+  )
+}
+
+export default SousAccueilCalculerDeveloppement;
---
+++ src/pages/CalculerDeveloppement/pageMachineCalculerDeveloppement.jsx
@@ -0,0 +1,17 @@
+import MachineExo from "../../MachineExo.jsx"; // Assure-toi que le chemin est correct
+
+function PageMachineCalculerDeveloppement() {
+  // 1. Tu définis la liste des niveaux de ce chapitre
+  const niveauxEquations = [
+    { id: 1, label: "Niveau 1 — Multiplications simples", pathRoute: "/PratiqueAutonomeTesterEgalite1" },
+   ];
+  // 2. Tu injectes ces données dans ton composant universel
+  return (
+    <MachineExo
+      titreModule="Chapitre : Calcul astucieux via le Développement"
+      listeNiveaux={niveauxEquations}
+    />
+  );
+}
+
+export default PageMachineCalculerDeveloppement;
