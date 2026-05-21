e175cc37d97f3faffaccab34379d02ba849b6a05
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-21 09:39:59 +0200

    correction de faute d'orthographe et syntaxe

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -27,7 +27,7 @@ function Accueil(){
           L'intelligence artificielle au service de ta réussite en mathématiques.
         </h4>
         <p className="text-muted small mb-5 px-3" style={{maxWidth: '600px'}}>
-          Un projet conçu par Morgan MARTIN — Professeur Agrégé de Mathématiques
+          Un projet conçu par Morgan MARTIN — Professeur agrégé de mathématiques
         </p>
 
         <div className="container px-0" style={{ maxWidth: '1200px' }}>
@@ -35,43 +35,43 @@ function Accueil(){
 
             <div className="col-12 col-md-6 col-xl-4 d-flex">
               <ModuleCard
-                titre="Module 1: Calculer astucieusement grâce au développement"
+                titre="Module 1 : Calculer astucieusement grâce au développement"
                 description="Transforme des multiplications complexes en opérations simples et super rapides de tête en apprenant à décomposer les nombres grâce à la distributivité !"
                 path="/SousAccueilCalculerDeveloppement"/>
             </div>
 
             <div className="col-12 col-md-6 col-xl-4 d-flex">
               <ModuleCard
-                titre="Module 2: Calculer astucieusement grâce à la factorisation"
+                titre="Module 2 : Calculer astucieusement grâce à la factorisation"
                 description="Repère le nombre qui se répète dans un calcul pour le mettre en facteur et regrouper les morceaux."
                 path="/SousAccueilCalculFactorisation"/>
             </div>
 
             <div className="col-12 col-md-6 col-xl-4 d-flex">
               <ModuleCard
-                titre="Module 3: Comprendre un programme de calcul"
+                titre="Module 3 : Comprendre un programme de calcul"
                 description="Apprends à construire des programmes de calcul en manipulant des cartes et valide chaque étape pour maîtriser le raisonnement logique."
                 path="/SousAccueilExoInteractif"/>
             </div>
 
             <div className="col-12 col-md-6 col-xl-4 d-flex">
               <ModuleCard
-                titre="Module 4: Introduction au calcul littéral"
-                description="Passer des phrases aux formules et comprendre le rôle des lettres."
+                titre="Module 4 : Introduction au calcul littéral"
+                description="Passe des phrases aux formules et comprends le rôle des lettres."
                 path="/SousAccueilCalculLitteral"
               />
             </div>
 
             <div className="col-12 col-md-6 col-xl-4 d-flex">
               <ModuleCard
-                titre="Module 5: Simplifier une expression littérale"
-                description="Écris plus vite, comme un pro ! Apprends à supprimer les signes 'x' inutiles pour raccourcir tes formules et simplifier tes calculs en un clin d'œil."
+                titre="Module 5 : Simplifier une expression littérale"
+                description="Écris plus vite, comme un pro ! Apprends à supprimer les signes « × » inutiles pour raccourcir tes formules et simplifier tes calculs en un clin d'œil."
                 path="/SousAccueilExpressionLitterale"/>
             </div>
 
             <div className="col-12 col-md-6 col-xl-4 d-flex">
               <ModuleCard
-                titre="Module 6: Tester une Égalité"
+                titre="Module 6 : Tester une égalité"
                 description="Apprends à remplacer les lettres par des nombres pour vérifier, étape par étape, si le membre de gauche et le membre de droite sont parfaitement égaux."
                 path="/SousAccueilTesterEgalite"
               />
@@ -79,8 +79,8 @@ function Accueil(){
 
             <div className="col-12 col-md-6 col-xl-4 d-flex">
               <ModuleCard
-                titre="Module 7: Résolution d'équation"
-                description="Apprendre à isoler x et maîtriser la balance algébrique."
+                titre="Module 7 : Résolution d'équations"
+                description="Apprends à isoler x et à maîtriser la balance algébrique."
                 path="/sousAccueilEquation"
               />
             </div>
