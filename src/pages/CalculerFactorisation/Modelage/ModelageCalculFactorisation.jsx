4b031cd0a1f8429e6b5bad04a12d6e0188dd9f27
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-21 10:51:18 +0200

    ajout de la video explicative de la factorisation

--- src/pages/CalculerFactorisation/Modelage/ModelageCalculFactorisation.jsx
+++ src/pages/CalculerFactorisation/Modelage/ModelageCalculFactorisation.jsx
@@ -69,7 +69,7 @@ function ModelageCalculFactorisation() {
             {afficheVideo && (
               <div className="mt-3">
                 <video controls width="100%" className="rounded-4">
-                  {/*<source src="/videos/Niveau-1-1-Introduction-au-calcul-litteral.mp4" type="video/mp4" />*/}
+                  <source src="/videos/Niveau-3-2-La-factorisation-pour-calculer.mp4" type="video/mp4" />
                   Votre navigateur ne supporte pas la lecture de vidéos.
                 </video>
               </div>
