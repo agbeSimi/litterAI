be0e0f06d6dd9fe75171bfdb687a2055c0d8c22d
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-26 15:21:15 +0200

    ajout des liens vers la pratique autonome dans leur niveau pour la machine a exercice

--- src/pages/DevelopperUneExpressionSimple/pageMachineDevExp.jsx
+++ src/pages/DevelopperUneExpressionSimple/pageMachineDevExp.jsx
@@ -3,9 +3,9 @@ import MachineExo from "../../MachineExo.jsx"; // Assure-toi que le chemin est c
 function PageMachineDevExp() {
   // 1. Tu définis la liste des niveaux de ce chapitre
   const niveaux = [
-    { id: 1, label: "Niveau 1 — Numérique", pathRoute: "" },
-    { id: 2, label: "Niveau 2 — Litéral simple", pathRoute: "" },
-    { id: 3, label: "Niveau 3 — Avancé ", pathRoute: "" }
+    { id: 1, label: "Niveau 1 — Numérique", pathRoute: "/PratiqueAutonomeDevExp1" },
+    { id: 2, label: "Niveau 2 — Litéral simple", pathRoute: "/PratiqueAutonomeDevExp2" },
+    { id: 3, label: "Niveau 3 — Avancé ", pathRoute: "/PratiqueAutonomeDevExp3" }
   ];
 
   // 2. Tu injectes ces données dans ton composant universel
