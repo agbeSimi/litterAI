e15ce77b54b83096f07abcd51f6d9ca30c66248d
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-27 14:59:45 +0200

    premiere page de connexion

---
+++ src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import {useState} from "react";
+import {handleSubmitLogin} from "../services/LitterAI_API.js";
+
+function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+
+
+
+
+  return(
+    <form onSubmit={event => {
+      handleSubmitLogin(event,userName,password)}}>
+      <input
+        type="text"
+        placeholder="Pseudo"
+        value={userName}
+        onChange={(event) => setUserName(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit">Se connecter</button>
+    </form>
+  )
+
+}
+
+export default Login;
