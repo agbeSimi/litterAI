3cdea37d263356d00fb88a36d1c72618239e65ee
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-05-28 10:44:45 +0200

    toute les routes ont maintenant besoin d'un token

--- src/App.jsx
+++ src/App.jsx
@@ -84,105 +84,110 @@ import PratiqueAutonomeDevExp2
 import PratiqueAutonomeDevExp3
   from "./pages/DevelopperUneExpressionSimple/PratiqueAutonome/pratiqueAutonomeDevExp3.jsx";
 import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./composants/ProtectedRoute.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Accueil/>}/>
-        <Route path="credit" element={<Credit/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
         <Route path="/Login" element={<Login/>}/>
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/" element={<Accueil/>}/>
+          <Route path="credit" element={<Credit/>}/>
+          <Route path="/Contact" element={<Contact/>}/>
+          <Route path="/machineExo" element={<MachineExo/>}/>
+
+          <Route path="/equation" element={<PageEquation/>}/>
+          <Route path="/sousAccueilEquation" element={<SousAccueilEquation/>}/>
+          <Route path="/PageMachineEquations" element={<PageMachineEquations/>}/>
+          <Route path="/modelage" element={<Modelage1/>}/>
+          <Route path="/modelage2" element={<Modelage2/>}/>
+          <Route path="/modelage3" element={<Modelage3/>}/>
+          <Route path="/pratiqueGuide" element={<PratiqueGuide1/>}/>
+          <Route path="/pratiqueGuide2" element={<PratiqueGuide2/>}/>
+          <Route path="/pratiqueGuide3" element={<PratiqueGuide3/>}/>
+          <Route path="/pratiqueAutonome" element={<PratiqueAutonome1/>}/>
+          <Route path="/pratiqueAutonome2" element={<PratiqueAutonome2/>}/>
+          <Route path="/pratiqueAutonome3" element={<PratiqueAutonome3/>}/>
 
 
-        <Route path="/machineExo" element={<MachineExo/>}/>
-
-        <Route path="/equation" element={<PageEquation/>}/>
-        <Route path="/sousAccueilEquation" element={<SousAccueilEquation/>}/>
-        <Route path="/PageMachineEquations" element={<PageMachineEquations/>}/>
-        <Route path="/modelage" element={<Modelage1/>}/>
-        <Route path="/modelage2" element={<Modelage2/>}/>
-        <Route path="/modelage3" element={<Modelage3/>}/>
-        <Route path="/pratiqueGuide" element={<PratiqueGuide1/>}/>
-        <Route path="/pratiqueGuide2" element={<PratiqueGuide2/>}/>
-        <Route path="/pratiqueGuide3" element={<PratiqueGuide3/>}/>
-        <Route path="/pratiqueAutonome" element={<PratiqueAutonome1/>}/>
-        <Route path="/pratiqueAutonome2" element={<PratiqueAutonome2/>}/>
-        <Route path="/pratiqueAutonome3" element={<PratiqueAutonome3/>}/>
-
-
-        <Route path="/calculLitteral" element={<CalculLiteral/>}/>
-        <Route path="/sousAccueilCalculLitteral" element={<SousAccueilCalculLitteral/>}/>
-        <Route path="/IntroductionCalculLitteral" element={<IntroductionCalculLitteral/>}/>
-        <Route path="/ModelageCalculLiteralPhase1" element={<ModelageCalculLiteralPhase1/>}/>
-        <Route path="/modelageCalculLiteralPhase2" element={<ModelageCalculLiteralPhase2/>}/>
-        <Route path="/modelageCalculLiteralPhase3" element={<ModelageCalculLiteralPhase3/>}/>
-        <Route path="pageMachineCalculLitteral" element={<PageMachineCalculLitteral/>}/>
-        <Route path="/PratiqueAutonomeCL1" element={<PratiqueAutonomeCL1/>}/>
-        <Route path="/PratiqueAutonomeCL2" element={<PratiqueAutonomeCL2/>}/>
-        <Route path="/PratiqueAutonomeCL3" element={<PratiqueAutonomeCL3/>}/>
-        <Route path="/PratiqueGuidePhase1CL" element={<PratiqueGuidePhase1CL/>}/>
-        <Route path="/pratiqueGuidePhase2CL" element={<PratiqueGuidePhase2CL/>}/>
-        <Route path="/pratiqueGuidePhase3CL" element={<PratiqueGuidePhase3CL/>}/>
+          <Route path="/calculLitteral" element={<CalculLiteral/>}/>
+          <Route path="/sousAccueilCalculLitteral" element={<SousAccueilCalculLitteral/>}/>
+          <Route path="/IntroductionCalculLitteral" element={<IntroductionCalculLitteral/>}/>
+          <Route path="/ModelageCalculLiteralPhase1" element={<ModelageCalculLiteralPhase1/>}/>
+          <Route path="/modelageCalculLiteralPhase2" element={<ModelageCalculLiteralPhase2/>}/>
+          <Route path="/modelageCalculLiteralPhase3" element={<ModelageCalculLiteralPhase3/>}/>
+          <Route path="pageMachineCalculLitteral" element={<PageMachineCalculLitteral/>}/>
+          <Route path="/PratiqueAutonomeCL1" element={<PratiqueAutonomeCL1/>}/>
+          <Route path="/PratiqueAutonomeCL2" element={<PratiqueAutonomeCL2/>}/>
+          <Route path="/PratiqueAutonomeCL3" element={<PratiqueAutonomeCL3/>}/>
+          <Route path="/PratiqueGuidePhase1CL" element={<PratiqueGuidePhase1CL/>}/>
+          <Route path="/pratiqueGuidePhase2CL" element={<PratiqueGuidePhase2CL/>}/>
+          <Route path="/pratiqueGuidePhase3CL" element={<PratiqueGuidePhase3CL/>}/>
 
 
 
-        <Route path="/sousAccueilExoInteractif" element={<SousAccueilExoInteractif/>}/>
-        <Route path="/modelageInteractif" element={<ModelageInteractif/>}/>
-        <Route path="/pageMachineExoInteractif" element={<PageMachineExoInteractif/>}/>
-        <Route path="/PratiqueGuideInteractif" element={<PratiqueGuideInteractif/>}/>
-        <Route path="/PratiqueAutonomeInteractif" element={<PratiqueAutonomeInteractif/>}/>
+          <Route path="/sousAccueilExoInteractif" element={<SousAccueilExoInteractif/>}/>
+          <Route path="/modelageInteractif" element={<ModelageInteractif/>}/>
+          <Route path="/pageMachineExoInteractif" element={<PageMachineExoInteractif/>}/>
+          <Route path="/PratiqueGuideInteractif" element={<PratiqueGuideInteractif/>}/>
+          <Route path="/PratiqueAutonomeInteractif" element={<PratiqueAutonomeInteractif/>}/>
 
 
-        <Route path="/IntroductionIAEL" element={<IntroductionIAEL/>}/>
-        <Route path="/SousAccueilExpressionLitterale" element={<SousAccueilExpressionLitterale/>}/>
-        <Route path="/pageMachineExpressionLitteral" element={<PageMachineExpressionLitteral/>}/>
-        <Route path="/ModelageExpressionLitterale" element={<ModelageExpressionLitteral/>}/>
-        <Route path="/PratiqueGuideEL" element={<PratiqueGuideEL/>}/>
-        <Route path="/pratiqueAutonomeEL" element={<PratiqueAutonomeEL/>}/>
+          <Route path="/IntroductionIAEL" element={<IntroductionIAEL/>}/>
+          <Route path="/SousAccueilExpressionLitterale" element={<SousAccueilExpressionLitterale/>}/>
+          <Route path="/pageMachineExpressionLitteral" element={<PageMachineExpressionLitteral/>}/>
+          <Route path="/ModelageExpressionLitterale" element={<ModelageExpressionLitteral/>}/>
+          <Route path="/PratiqueGuideEL" element={<PratiqueGuideEL/>}/>
+          <Route path="/pratiqueAutonomeEL" element={<PratiqueAutonomeEL/>}/>
 
 
-        <Route path="/introductionEgalite" element={<IntroductionEgalite/>}/>
-        <Route path="/SousAccueilTesterEgalite" element={<SousAccueilTesterEgalite/>}/>
-        <Route path="pageMachineTesterEgalite" element={<PageMachineTesterEgalite/>}/>
-        <Route path="/TesterEgalite" element={<TesterEgalite/>}/>
+          <Route path="/introductionEgalite" element={<IntroductionEgalite/>}/>
+          <Route path="/SousAccueilTesterEgalite" element={<SousAccueilTesterEgalite/>}/>
+          <Route path="pageMachineTesterEgalite" element={<PageMachineTesterEgalite/>}/>
+          <Route path="/TesterEgalite" element={<TesterEgalite/>}/>
 
-        <Route path="/ModelageTesterEgalite1" element={<ModelageTesterEgalite1/>}/>
-        <Route path="/PratiqueGuideTesterEgalite1" element={<PratiqueGuideTesterEgalite1/>}/>
-        <Route path="/PratiqueGuideTesterEgalite2" element={<PratiqueGuideTesterEgalite2/>}/>
-        <Route path="/PratiqueGuideTesterEgalite3" element={<PratiqueGuideTesterEgalite3/>}/>
-        <Route path="/PratiqueAutonomeTesterEgalite1" element={<PratiqueAutonomeTesterEgalite1/>}/>
-        <Route path="/PratiqueAutonomeTesterEgalite2" element={<PratiqueAutonomeTesterEgalite2/>}/>
-        <Route path="/PratiqueAutonomeTesterEgalite3" element={<PratiqueAutonomeTesterEgalite3/>}/>
+          <Route path="/ModelageTesterEgalite1" element={<ModelageTesterEgalite1/>}/>
+          <Route path="/PratiqueGuideTesterEgalite1" element={<PratiqueGuideTesterEgalite1/>}/>
+          <Route path="/PratiqueGuideTesterEgalite2" element={<PratiqueGuideTesterEgalite2/>}/>
+          <Route path="/PratiqueGuideTesterEgalite3" element={<PratiqueGuideTesterEgalite3/>}/>
+          <Route path="/PratiqueAutonomeTesterEgalite1" element={<PratiqueAutonomeTesterEgalite1/>}/>
+          <Route path="/PratiqueAutonomeTesterEgalite2" element={<PratiqueAutonomeTesterEgalite2/>}/>
+          <Route path="/PratiqueAutonomeTesterEgalite3" element={<PratiqueAutonomeTesterEgalite3/>}/>
 
-        <Route path="/IntroductionCalculerDeveloppement" element={<IntroductionCalculerDeveloppement/>}/>
-        <Route path="/SousAccueilCalculerDeveloppement" element={<SousAccueilCalculerDeveloppement/>}/>
-        <Route path="/PageMachineCalculerDeveloppement" element={<PageMachineCalculerDeveloppement/>}/>
-        <Route path="/ModelageCalculDeveloppement" element={<ModelageCalculDeveloppement/>}/>
-        <Route path="/PratiqueGuideCalculDeveloppement" element={<PratiqueGuideCalculDeveloppement/>}/>
-        <Route path="/PratiqueAutonomeDeveloppement" element={<PratiqueAutonomeDeveloppement/>}/>
+          <Route path="/IntroductionCalculerDeveloppement" element={<IntroductionCalculerDeveloppement/>}/>
+          <Route path="/SousAccueilCalculerDeveloppement" element={<SousAccueilCalculerDeveloppement/>}/>
+          <Route path="/PageMachineCalculerDeveloppement" element={<PageMachineCalculerDeveloppement/>}/>
+          <Route path="/ModelageCalculDeveloppement" element={<ModelageCalculDeveloppement/>}/>
+          <Route path="/PratiqueGuideCalculDeveloppement" element={<PratiqueGuideCalculDeveloppement/>}/>
+          <Route path="/PratiqueAutonomeDeveloppement" element={<PratiqueAutonomeDeveloppement/>}/>
 
 
-        <Route path="/IntroductionCalculFactorisation" element={<IntroductionCalculFactorisation/>}/>
-        <Route path="/SousAccueilCalculFactorisation" element={<SousAccueilCalculFactorisation/>}/>
-        <Route path="/PageMachineCalculFactorisation" element={<PageMachineCalculFactorisation/>}/>
-        <Route path="/ModelageCalculFactorisation" element={<ModelageCalculFactorisation/>}/>
-        <Route path="/PratiqueGuideCalculFactorisation" element={<PratiqueGuideCalculFactorisation/>}/>
-        <Route path="/PratiqueAutonomeFactorisation" element={<PratiqueAutonomeFactorisation/>}/>
+          <Route path="/IntroductionCalculFactorisation" element={<IntroductionCalculFactorisation/>}/>
+          <Route path="/SousAccueilCalculFactorisation" element={<SousAccueilCalculFactorisation/>}/>
+          <Route path="/PageMachineCalculFactorisation" element={<PageMachineCalculFactorisation/>}/>
+          <Route path="/ModelageCalculFactorisation" element={<ModelageCalculFactorisation/>}/>
+          <Route path="/PratiqueGuideCalculFactorisation" element={<PratiqueGuideCalculFactorisation/>}/>
+          <Route path="/PratiqueAutonomeFactorisation" element={<PratiqueAutonomeFactorisation/>}/>
 
-        <Route path="/PageMachineDevExp" element={<PageMachineDevExp/>}/>
-        <Route path="/SousAccueilDevExp" element={<SousAccueilDevExp/>}/>
-        <Route path="/IntroductionDevExp" element={<IntroductionDevExp/>}/>
-        <Route path="/DevExp" element={<DevExp/>}/>
-        <Route path="/ModelageDevExp" element={<ModelageDevExp/>}/>
-        <Route path="/ModelageDevExp2" element={<ModelageDevExp2/>}/>
-        <Route path="/ModelageDevExp3" element={<ModelageDevExp3/>}/>
-        <Route path="/PratiqueGuideDevExpP1" element={<PratiqueGuideDevExpP1/>}/>
-        <Route path="/PratiqueGuideDevExpP2" element={<PratiqueGuideDevExpP2/>}/>
-        <Route path="/PratiqueGuideDevExpP3" element={<PratiqueGuideDevExpP3/>}/>
-        <Route path="/PratiqueAutonomeDevExp1" element={<PratiqueAutonomeDevExp1/>}/>
-        <Route path="/PratiqueAutonomeDevExp2" element={<PratiqueAutonomeDevExp2/>}/>
-        <Route path="/PratiqueAutonomeDevExp3" element={<PratiqueAutonomeDevExp3/>}/>
+          <Route path="/PageMachineDevExp" element={<PageMachineDevExp/>}/>
+          <Route path="/SousAccueilDevExp" element={<SousAccueilDevExp/>}/>
+          <Route path="/IntroductionDevExp" element={<IntroductionDevExp/>}/>
+          <Route path="/DevExp" element={<DevExp/>}/>
+          <Route path="/ModelageDevExp" element={<ModelageDevExp/>}/>
+          <Route path="/ModelageDevExp2" element={<ModelageDevExp2/>}/>
+          <Route path="/ModelageDevExp3" element={<ModelageDevExp3/>}/>
+          <Route path="/PratiqueGuideDevExpP1" element={<PratiqueGuideDevExpP1/>}/>
+          <Route path="/PratiqueGuideDevExpP2" element={<PratiqueGuideDevExpP2/>}/>
+          <Route path="/PratiqueGuideDevExpP3" element={<PratiqueGuideDevExpP3/>}/>
+          <Route path="/PratiqueAutonomeDevExp1" element={<PratiqueAutonomeDevExp1/>}/>
+          <Route path="/PratiqueAutonomeDevExp2" element={<PratiqueAutonomeDevExp2/>}/>
+          <Route path="/PratiqueAutonomeDevExp3" element={<PratiqueAutonomeDevExp3/>}/>
+
+        </Route>
+
+
+
 
 
 
