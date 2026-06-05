b0ae4ead123546e68e80d581be5b22dc3a6da90a
agbe0001 <simisola.agbe@etudiant.univ-reims.fr>
2026-06-05 11:01:11 +0200

    classe inutile

--- src/composants/BoutonSuivant.jsx
+++ src/composants/BoutonSuivant.jsx
@@ -1,18 +1,18 @@
-
-
-function boutonSuivant({onClick, visible}) {
-  if(!visible) return null
-    return (
-      <div className="d-flex justify-content-center"> {/* Pour centrer le bouton sous le bloc */}
-        <button
-          className="btn btn-primary border-0 rounded-circle shadow d-flex align-items-center justify-content-center "
-          onClick={onClick}
-          style={{ width: '50px', height: '50px' }} // On force la taille pour faire un cercle parfait
-        >
-          <span style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>→</span>
-        </button>
-      </div>
-  );
-}
-
-export default boutonSuivant;
+//
+//
+// function boutonSuivant({onClick, visible}) {
+//   if(!visible) return null
+//     return (
+//       <div className="d-flex justify-content-center"> {/* Pour centrer le bouton sous le bloc */}
+//         <button
+//           className="btn btn-primary border-0 rounded-circle shadow d-flex align-items-center justify-content-center "
+//           onClick={onClick}
+//           style={{ width: '50px', height: '50px' }} // On force la taille pour faire un cercle parfait
+//         >
+//           <span style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>→</span>
+//         </button>
+//       </div>
+//   );
+// }
+//
+// export default boutonSuivant;
